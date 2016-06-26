9c49aa5c1cfb347aec51ce0f783631c423d7875f
alomina007 <alomina007@users.noreply.github.com>
2016-06-26 08:11:52 +0430

    Update v-card.lua

--- plugins/v-card.lua
+++ plugins/v-card.lua
@@ -3,27 +3,11 @@ do
 local text = [[ 
 Team member ID card
 _____________________
-Name: Arash Ranger
-Username: @ArashRanger
-Rank: Sudo,Chief
-Phone number: +989396017398
------------------------
-Name: Shervin Sheriff
-Username: @hacker_unknown
-Rank: Admin,Manager
-Phone number: ---
------------------------
-Name: Dr Cyber
-Username: @DrCyber1736
-Rank: Admin,Protecter
-Phone number: ---
------------------------
-Name: Amir Sbss
-Username: @Amir_h
-Rank: Admin,Helper
-Phone number: ---
-______________________
-نکته:این افراد تنها ادمین ها و اعضای رسمی تیم هستند
+Name: spammer
+Username: @spammer021
+Rank: Sudo⭐⭐⭐⭐⭐
+Phone number: +16625944675
+این فرد سودواصلی ربات هست
 ]]
 return text
 end
