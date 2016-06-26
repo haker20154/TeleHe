ef3f9f7d287c49a0ac2069ff623d73c33b1dbec5
alomina007 <alomina007@users.noreply.github.com>
2016-06-26 08:01:30 +0430

    Update weather.lua

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -28,7 +28,7 @@ local function get_weather(location)
     conditions = conditions .. 'مه 🌫'
   end
 
-  return temp .. '\n\n' .. conditions..'\n\n@avast_Team'
+  return temp .. '\n\n' .. conditions..'\n\n@➡Alominateam⬅'
 end
 local function run(msg, matches) 
     city = matches[1]
