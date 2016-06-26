489992b64f583b1d618436a7794c69a03a15163c
alomina007 <alomina007@users.noreply.github.com>
2016-06-26 08:50:25 +0430

    Update botsettings.lua

--- plugins/botsettings.lua
+++ plugins/botsettings.lua
@@ -2,17 +2,13 @@ do
  function run(msg, matches)
 return [[ 
 تنظیمات ربات:
-نام: 【Asimo B✪T】
-آیدی: 189868992
-نام کاربری: ندارد
+نام: 【ALomina】
 
-سودو: @ArashRanger ( 226726601 )
+نام کاربری:Alomina
+
+سودو: @spammer021 ( 97715211 )
 مدیران:
-@hacker_unknown ( 149228796 )
-@Sudo_Vira_DrCyber ( 239832443 )
-@Amir_h ( 122774063 )
-@MR_vViruSs ( 223216007 )
-تصویر پروفایل: http://yon.ir/btwL
+این بخش هنوزتکمیل نیست
 ]]
 end
 return {
